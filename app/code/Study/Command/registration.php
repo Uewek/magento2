9bf36bec4332a5b8c1e865be0d3f22c6a19fce72
Mykola Haidenok <uewek87@gmail.com>
2021-09-30 10:14:43 +0300

    console command

---
+++ app/code/Study/Command/registration.php
@@ -0,0 +1,7 @@
+<?php
+declare(strict_types=1);
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Study_Command',
+    __DIR__
+);
