2815852b33b2b4af396ef797f9c9178e5c37c760
Mykola Haidenok <uewek87@gmail.com>
2021-11-03 20:40:13 +0200

    fixed comments

--- app/code/Study/CategoryExternalCode/ViewModel/CategoryExternalCodeViewModel.php
+++ app/code/Study/CategoryExternalCode/ViewModel/CategoryExternalCodeViewModel.php
@@ -5,20 +5,33 @@ namespace Study\CategoryExternalCode\ViewModel;
 use Magento\Catalog\Helper\Data;
 use Magento\Framework\View\Element\Block\ArgumentInterface;
 
+/**
+ * View model transfer data to storefront category page
+ */
 class CategoryExternalCodeViewModel implements ArgumentInterface
 {
+    /**
+     * @var Data
+     */
+    private $dataHelper;
 
+    /**
+     * Class constructor
+     *
+     * @param Data $dataHelper
+     */
     public function __construct(
         Data $dataHelper
     ) {
         $this->dataHelper = $dataHelper;
-
     }
+
     /**
      * Return external code for template
-
+     *
+     * @return string
      */
-    public function getExternalCode()
+    public function getExternalCode(): string
     {
         $category = $this->dataHelper->getCategory();
         $externalCode = $category->getExtensionAttributes()->getCategoryExternalCode();
@@ -29,5 +42,4 @@ class CategoryExternalCodeViewModel implements ArgumentInterface
 
         return $resultCode;
     }
-
 }
